796683eed29c1e68a302270025e7cad0550d2030
ramaninidhi <ramaninidhi114@gmail.com>
2016-09-14 11:29:06 +0530

    firstcommit

---
+++ routes/submitLogIn.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+  res.render('submitLogIn', { usertype: 'normal' });
+
+});
+
+module.exports = router;
